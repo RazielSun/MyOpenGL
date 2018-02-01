b044a5dc0f87d868c1f587c0ffd2312944de68a5
Alexandr Kardakov <razielsun@gmail.com>
2018-02-01 16:48:19 +0300

    update optim fixes

--- chapter01/main.cpp
+++ chapter01/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 const TGAColor white = TGAColor (255, 255, 255, 255);
 const TGAColor red   = TGAColor (255, 0,   0,   255);
 
-void draw_line (int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
+void draw_line (int x0, int y0, int x1, int y1, TGAImage& image, const TGAColor& color) {
 
 	bool inverted = false;
 
@@ -20,14 +20,26 @@ void draw_line (int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
 		swap(y0, y1);
 	}
 
+	int dx = x1 - x0;
+	int dy = y1 - y0;
+	int delta = dx * 2;
+	int derror = abs(dy) * 2;
+	int error = 0;
+	int y = y0;
+
 	for (int x = x0; x <= x1; ++x) {
-		float t = (x - x0) / (float) (x1 - x0);
-		int y = (1.0f - t) * y0 + t * y1;
 		if (inverted) {
 			image.set (y, x, color);
 		}
 		else {
 			image.set (x, y, color);
+		}
+
+		error += derror;
+
+		if (error > dx) {
+			y += (y0<y1) ? 1 : -1;
+			error -= delta;
 		}
 	}
 }
--- chapter01/optim.sh
+++ chapter01/optim.sh
@@ -3,4 +3,5 @@
 set -e
 cd `dirname $0`/
 
-g++ -ggdb -g3 -pg -O0 main.cpp tgaimage.cpp -o main
+# g++ -ggdb -g3 -pg -O0 main.cpp tgaimage.cpp -o main
+g++ -O3 main.cpp tgaimage.cpp -o main
